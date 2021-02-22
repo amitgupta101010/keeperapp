519177bb97db4180ef96fa25adf5e56852b47462
AmitKumarWG <Amit.Kumar@watchguard.com>
2021-02-22 11:39:24 +0530

    Updates

---
+++ precache-manifest.4816e01d99cbb3b8e0b93331565bf833.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686880de75c185fc97a9b8d6cee21332",
+    "url": "/keeperapp/index.html"
+  },
+  {
+    "revision": "3a5563ad6d89d5b29ba0",
+    "url": "/keeperapp/static/js/2.64d6c001.chunk.js"
+  },
+  {
+    "revision": "23b8ff54507dba089902",
+    "url": "/keeperapp/static/js/main.bccd2918.chunk.js"
+  },
+  {
+    "revision": "1d2499a61e6564ef567e",
+    "url": "/keeperapp/static/js/runtime-main.aad2a9b1.js"
+  }
+]);
